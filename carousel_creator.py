9f68170c4b9a496c4f84033edee82bac89b3325e
nighthawk-30 <diencurtis@twistedloop.com>
2025-06-10 15:43:54 +0100

    Update carousel_creator.py

--- carousel_creator.py
+++ carousel_creator.py
@@ -62,7 +62,7 @@ Content:
 """
 
             response = client.chat.completions.create(
-                model="gpt-4",
+                model="gpt-4o",
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.7,
             )
